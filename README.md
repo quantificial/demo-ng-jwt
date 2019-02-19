1dce2a5ee95e2c4c01050dab96d5b6b445c9e5c7
Johnson Fu <johnson.fu@chubb.com>
2019-02-19 18:07:46 +0800

    update

--- README.md
+++ README.md
@@ -1,3 +1,33 @@
+# demo of using angular + backend api + jwt auth
+
 This sample is published as part of the corresponding blog article at [https://www.toptal.com/angular/angular-6-jwt-authentication](https://www.toptal.com/angular/angular-6-jwt-authentication).
 
 Visit https://www.toptal.com/blog and subscribe to our newsletter to read great posts!
+
+# server
+need to add the jwt package
+
+npm install @auth0/angular-jwt
+
+need to install body-parser, jsonwebtoken and express-jwt
+
+npm install body-parser jsonwebtoken express-jwt
+
+- body-parser is used to analyze, and could process different kind of response type, such as text, json, urlendcoded and could also handle different encoding
+
+# sign and create the token
+```
+    var token = jwt.sign({userID: user.id}, secretKey, {expiresIn: '2h'});
+```
+
+## auth
+
+http://localhost:4000/api/auth/
+
+username and password
+
+## Get Todos
+http://localhost:4000/api/todos
+
+header: Authorization: Bearer {token}
+
--- server/app.js
+++ server/app.js
@@ -4,6 +4,36 @@ const app = express()
 const bodyParser = require('body-parser');
 const jwt = require('jsonwebtoken');
 const expressJwt = require('express-jwt');
+var winston = require('winston');
+const {transports, createLogger, format} = require('winston');
+
+const logger = winston.createLogger({
+    level: 'info',
+    format: format.combine(
+        format.timestamp(),
+        format.json()
+    ),
+    defaultMeta: { service: 'user-service' },
+    transports: [
+      //
+      // - Write to all logs with level `info` and below to `combined.log` 
+      // - Write all logs error (and below) to `error.log`.
+      //      
+      //new (winston.transports.File)({'timestamp':true}),
+      new winston.transports.File({ filename: 'error.log', level: 'error', 'timestamp': true }),
+      new winston.transports.File({ filename: 'combined.log' })
+    ]
+  });
+
+//
+// If we're not in production then log to the `console` with the format:
+// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
+// 
+if (process.env.NODE_ENV !== 'production') {
+    logger.add(new winston.transports.Console({
+      format: winston.format.simple()
+    }));
+  }  
 
 var TODOS = [
     { 'id': 1, 'user_id': 1, 'name': "Get Milk", 'completed': false },
@@ -30,30 +60,58 @@ function getUsers() {
     return USERS;
 }
 
+// support json body
 app.use(bodyParser.json());
-app.use(expressJwt({secret: 'todo-app-super-shared-secret'}).unless({path: ['/api/auth']}));
+
+// support urlencoded form
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// the secret
+
+const secretKey = 'qwertyuiop[]12345567890-='
+
+app.use(expressJwt({secret: secretKey}).unless({path: ['/api/auth']}));
+
+// express jwt error handling
+app.use(function(err, req, res, next) {
+    if(err.name === 'UnauthorizedError') {
+      res.status(err.status).send({message:err.message});
+      logger.error(err);
+      return;
+    }
+ next();
+});
+
+
 
 app.get('/', function (req, res) {
     res.send('Angular JWT Todo API Server')
 });
+
+// get jwt token
 app.post('/api/auth', function(req, res) {
     const body = req.body;
+    console.log(req.body);
 
     const user = USERS.find(user => user.username == body.username);
     if(!user || body.password != 'todo') return res.sendStatus(401);
     
-    var token = jwt.sign({userID: user.id}, 'todo-app-super-shared-secret', {expiresIn: '2h'});
+    // jwt.sign( payload, secret or key, options and callback)
+    var token = jwt.sign({userID: user.id}, secretKey, {expiresIn: '2h'});
     res.send({token});
 });
+
 app.get('/api/todos', function (req, res) {
     res.type("json");
     res.send(getTodos(req.user.userID));
 });
+
 app.get('/api/todos/:id', function (req, res) {
     var todoID = req.params.id;
     res.type("json");
     res.send(getTodo(todoID));
 });
+
 app.get('/api/users', function (req, res) {
     res.type("json");
     res.send(getUsers());
